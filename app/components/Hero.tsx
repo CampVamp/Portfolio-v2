3631b637072b04886e7f6ea51cf4ef1fd885479b
Ajay <ajayramsaravanan4@gmail.com>
2024-03-18 01:04:07 +0530

    feat: make site responsive

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,8 +1,8 @@
 const Hero = () => {
   return (
-    <div className="h-screen w-full flex items-center px-12">
-      <div className="flex flex-col justify-center gap-8">
-        <div className="text-9xl font-semibold text-neonGreen flex flex-col gap-4">
+    <div className="h-screen w-full flex items-center px-6 md:px-12">
+      <div className="flex flex-col justify-center gap-4 md:gap-8">
+        <div className="text-6xl md:text-9xl font-semibold text-neonGreen flex flex-col gap-4">
           <div>Hello</div>
           <div>My Name is</div>
           <div className="text-[#c4d4b4]">Ajay ram</div>
