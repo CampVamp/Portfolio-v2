75e74d0a080c6bbb7ea275df9aab8744c52560c6
Ajay <ajayramsaravanan4@gmail.com>
2024-03-18 00:42:00 +0530

    feat: add paper plane animation

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,8 +1,47 @@
+"use client";
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import PaperPlane from "@/public/paper-plane.png";
+import Image from "next/image";
+
 const Hero = () => {
+  gsap.registerPlugin(MotionPathPlugin);
+  const paperPlaneRef = useRef(null);
+  const flightPath = {
+    curviness: 1.25,
+    autoRotate: true,
+    values: [
+      { x: 100, y: -20 },
+      { x: 300, y: 10 },
+      { x: 500, y: 100 },
+      { x: 750, y: -100 },
+      { x: 350, y: -50 },
+      { x: 600, y: 100 },
+      { x: 800, y: 0 },
+      { x: window.innerWidth - 600, y: -250 },
+      { x: window.innerWidth - 500, y: -100 },
+    ],
+  };
+  useGSAP(() => {
+    const tween = gsap.to(paperPlaneRef.current, {
+      duration: 10,
+      motionPath: {
+        path: flightPath.values,
+        curviness: flightPath.curviness,
+        autoRotate: flightPath.autoRotate,
+      },
+      ease: "power1.inOut",
+      // repeat: -1,
+      // yoyo: true,
+    });
+  });
+
   return (
-    <div className="h-screen w-full flex items-center px-12">
-      <div className="flex flex-col justify-center gap-8">
-        <div className="text-9xl font-semibold text-neonGreen flex flex-col gap-4">
+    <div className="relative h-screen w-full flex items-center px-6 md:px-12">
+      <div className="flex flex-col justify-center gap-4 md:gap-8">
+        <div className="text-5xl md:text-9xl font-semibold text-neonGreen flex flex-col gap-4">
           <div>Hello</div>
           <div>My Name is</div>
           <div className="text-[#c4d4b4]">Ajay ram</div>
@@ -10,6 +49,9 @@ const Hero = () => {
         <div className="text-stone-500 text-3xl font-medium">
           Full Stack Developer
         </div>
+      </div>
+      <div className="absolute h-72 w-72">
+        <Image src={PaperPlane} alt="Plane" fill ref={paperPlaneRef} />
       </div>
     </div>
   );
